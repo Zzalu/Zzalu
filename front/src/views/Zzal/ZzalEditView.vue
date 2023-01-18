b89d4ad373c0cffe0e07cd560c9d380ef7fcd597
giokim1011 <giokim1011@gmail.com>
2023-01-18 17:46:57 +0900

    Init: 컴포넌트 구조 짜기
    view 파일 완료, components 추가 필요

---
+++ front/src/views/Zzal/ZzalEditView.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>네브바</h1>
+</template>
+
+<script>
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+  setup() {
+    
+  },
+})
+</script>
+
+<style scoped>
+
+</style>
