be0cac2dfce355186d26c5fbe3474da8105c129f
김지오 <giokim1011@gmail.com>
2023-01-23 17:04:37 +0900

    style: 폰트, 로고, 색깔 추가
    app.vue, helloworld.vue, config 파일 수정

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,26 +1,17 @@
 <template>
   <div>
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <h1 class="text-3xl font-bold underline bg-red-500">
-      테일윈드 해치웠나?
-    </h1>
+    
   </div>
 </template>
 
 <script>
+
 export default {
   name: 'App',
   components: {},
 };
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="postcss">
+
 </style>
