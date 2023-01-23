e4771c4e2cf23259877f304665daacf03c9614a2
ats166 <45484403+ats166@users.noreply.github.com>
2023-01-23 18:15:03 +0900

    design : 고독방 리스트 디자인ing

--- front/src/views/QuietChat/QuietChatListView.vue
+++ front/src/views/QuietChat/QuietChatListView.vue
@@ -1,13 +1,17 @@
 <template>
   <div>
     <div class="menu1">
-      <div>전체 고독방</div>
-      <div>내가 개설한</div>
-      <div>즐겨찾기</div>
+      <button>전체 고독방</button>
+      <button>내가 개설한</button>
+      <button>즐겨찾기</button>
     </div>
     <div class="menu2">
-      <div>최신 대화순</div>
-      <div>좋아요 순</div>
+      <button>최신 대화순</button>
+      <button>좋아요 순</button>
+    </div>
+
+    <div class="createchat">
+      <div class="testtext">새로운 고독방 개설하기</div>
     </div>
   </div>
 </template>
@@ -20,9 +24,15 @@ export default {
 
 <style scoped lang="postcss">
 .menu1 {
-  @apply grid grid-cols-3 text-center
+  @apply grid grid-cols-3 text-center border-b-4
 }
 .menu2 {
-  @apply grid grid-cols-2 text-center
+  @apply grid grid-cols-2 text-center border-t-4
+}
+.createchat{
+  @apply bg-zz-p rounded-lg w-80 h-10 text-center text-white 
+}
+.testtext {
+  @apply inline-block align-middle
 }
 </style>
