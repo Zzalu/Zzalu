e486a890c460f3fabc4dd529dfcfce46e86ac849
canbe159 <canbe159@gmail.com>
2023-01-24 21:20:23 +0900

    feature : RedisConfig 레디스 연결

--- backend/zzalu/src/main/java/com/example/zzalu/User/model/User.java
+++ backend/zzalu/src/main/java/com/example/zzalu/User/model/User.java
@@ -2,13 +2,18 @@ package com.example.zzalu.User.model;
 
 
 import com.example.zzalu.TitleHakwon.model.TitleHakwonComent;
+import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.*;
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
+@Getter
+@Setter
 @Entity
-public class User {
+public class User implements Serializable {
 
     @Id
     @Column(name="USER_ID")
@@ -17,7 +22,7 @@ public class User {
     String email;
     String password;
 
-    @OneToMany (mappedBy = "user")
+    @OneToMany (mappedBy = "user" ,cascade = CascadeType.ALL)
     private List<TitleHakwonComent> titleHakwonComents = new ArrayList<>();
 
 }
