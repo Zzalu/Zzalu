53f1d5688a79b25519e2f266f999865ff1ac5b56
chaeeun <codms1104@naver.com>
2023-01-25 14:31:40 +0900

    add: fontawesome

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,28 +1,29 @@
 <template>
-  <!-- <div class=nav>
-    <router-link :to="{name: 'HelloWorldView'}">하이</router-link>
-
-  </div> -->
-  <div class="main-margin">
-    <router-view></router-view>
+  <div>
+    <div class="main-margin">
+      <router-view></router-view>
+    </div>
+    <!-- <main-lower-nav-bar></main-lower-nav-bar> -->
+    <!-- <font-awesome-icon icon="fa-solid fa-house" /> -->
   </div>
 </template>
 
 <script>
-
+// import MainLowerNavBar from '@/components/Common/NavBar/MainLowerNavBar.vue';
 export default {
   name: 'App',
-  components: {},
-  methods: {
+  components: {
+    // MainLowerNavBar,
   },
+  methods: {},
 };
 </script>
 
 <style>
-.main-margin{
+.main-margin {
   padding: 0px;
-  margin-top : 3.5rem;
-  margin-left : 1.25rem;
-  margin-right : 1.25rem;
+  margin-top: 3.5rem;
+  margin-left: 1.25rem;
+  margin-right: 1.25rem;
 }
 </style>
--- front/src/main.js
+++ front/src/main.js
@@ -1,6 +1,23 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import "./css/common.css"
-import router from './router.js'
+import { createApp } from 'vue';
+import App from './App.vue';
+import './css/common.css';
+import router from './router.js';
 
-createApp(App).use(router).mount('#app')
+import { library } from '@fortawesome/fontawesome-svg-core';
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+/* import specific icons */
+// import { faHouse } from '@fortawesome/free-solid-svg-icons';
+
+import { fas } from '@fortawesome/free-solid-svg-icons';
+library.add(fas);
+import { fab } from '@fortawesome/free-brands-svg-icons';
+library.add(fab);
+import { far } from '@fortawesome/free-regular-svg-icons';
+library.add(far);
+
+/* add icons to the library */
+// library.add(faHouse);
+createApp(App).use(router).component('font-awesome-icon', FontAwesomeIcon).mount('#app');
