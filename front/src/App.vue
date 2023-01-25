7348e9d502b14d55ca3b94f3d1d33c88b9301152
chaeeun <codms1104@naver.com>
2023-01-25 23:31:35 +0900

    feature: navbar 추가

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,13 +1,16 @@
 <template>
   <!-- <div class="flex flex-col"> -->
   <div>
-    <div class="h-nav-height sticky inset-x-0 top-0 bg-white">top navbar</div>
+    <!-- <div class="h-nav-height sticky inset-x-0 top-0 bg-white">top navbar</div> -->
+    <go-back-top-nav-bar></go-back-top-nav-bar>
+    <logo-top-nav-bar></logo-top-nav-bar>
     <div class="main-margin">
       <div>
         <router-view></router-view>
       </div>
     </div>
-    <main-lower-nav-bar class="fixed inset-x-0 bottom-0"></main-lower-nav-bar>
+    <main-lower-nav-bar></main-lower-nav-bar>
+    <account-lower-nav-bar></account-lower-nav-bar>
     <!-- <main-lower-nav-bar></main-lower-nav-bar> -->
     <!-- <main-lower-nav-bar class="fixed bottom-0 left-0 bg-blue-600"></main-lower-nav-bar> -->
   </div>
@@ -15,10 +18,16 @@
 
 <script>
 import MainLowerNavBar from '@/components/Common/NavBar/MainLowerNavBar.vue';
+import GoBackTopNavBar from '@/components/Common/NavBar/GoBackTopNavBar.vue';
+import AccountLowerNavBar from './components/Common/NavBar/AccountLowerNavBar.vue';
+import LogoTopNavBar from './components/Common/NavBar/LogoTopNavBar.vue';
 export default {
   name: 'App',
   components: {
     MainLowerNavBar,
+    GoBackTopNavBar,
+    AccountLowerNavBar,
+    LogoTopNavBar,
   },
   methods: {},
 };
--- front/src/components/Common/NavBar/GoBackTopNavBar.vue
+++ front/src/components/Common/NavBar/GoBackTopNavBar.vue
@@ -1,13 +1,16 @@
 <template>
-  <div>뒤로가기 위쪽 네브</div>
+  <!-- 뒤로가기 nav -->
+  <div class="h-nav-height fixed inset-x-0 top-0 bg-white flex items-center">
+    <span class="inline-block px-4">
+      <font-awesome-icon icon="fa-solid fa-chevron-left" class="text-2xl" />
+    </span>
+  </div>
 </template>
 
 <script>
 export default {
-
-}
+  name: 'GoBackTopNavBar',
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
