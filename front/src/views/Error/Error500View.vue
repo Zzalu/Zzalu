60a6930f0b37ef9dfd42e0059eb1e84ebbbf3b16
giokim12 <109334986+giokim12@users.noreply.github.com>
2023-01-27 16:59:58 +0900

    Design: 에러페이지 디자인 완료
    공통 css 파일 추가

--- front/src/views/Error/Error500View.vue
+++ front/src/views/Error/Error500View.vue
@@ -1,13 +1,15 @@
 <template>
   <div class="text-center-container">
-    <div class="error-page-title">500</div>
-    <div class="sad-face">:(</div>
-    <div class="resting-face">:|</div>
-    <div class="error-page-sub-title">서비스 이용에 불편을 드려 죄송합니다.</div>
-    <div class="error-age-content">
+    <div class="error-title-container">
+      <div class="error-title">500</div>
+      <div class="sad-face">:(</div>
+      <div class="resting-face">:|</div>
+    </div>
+    <div class="error-sub-title">서비스 이용에 불편을 드려 죄송합니다.</div>
+    <div class="error-content">
       시스템 에러가 발생하여 해당 페이지를 표시할 수 없습니다.
     </div>
-    <div class="error-page-content">
+    <div class="error-content">
       궁금한 점이 있으시면 고객센터를 통해 문의해주시기 바랍니다.
     </div>
     <button class="go-back-button">이전 페이지</button>
@@ -27,17 +29,17 @@ export default {
 </script>
 
 <style scoped lang='postcss'>
-  @import "./errors.css";
+
 
   .sad-face {
     transform: rotate(90deg);
-    @apply absolute right-28 mt-28 font-pop text-4xl
+    left: 5.5rem;
+    @apply relative top-8 font-pop text-4xl
   }
 
   .resting-face {
     transform: rotate(90deg);
-    top: 11rem;
-    @apply absolute right-5 font-pop text-4xl
+    @apply relative right-4 top-4 font-pop text-4xl
   }
 
 </style>
