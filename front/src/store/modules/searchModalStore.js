779900ff1d845cc0f806bfa79118fd9e143a263c
ats166 <ats166@naver.com>
2023-01-30 17:31:54 +0900

    S08P12C109-158 Feat. 외부클릭시 보관함리스트 닫힘

--- front/src/store/modules/searchModalStore.js
+++ front/src/store/modules/searchModalStore.js
@@ -1,14 +1,21 @@
 const searchModalStore = {
-  namespace: true,
+  namespaced: true,
   state:() => ({
       //하단내브바 서치모달
-      open_search_modal: true,
+      open_search_modal: false,
+      open_list_modal: false,
     }),
   mutations: {
     //하단내브바 서치모달
-    open_modal(state) {
-      state.open_search_modal ? false : true;
+    open_search_modal(state) {
+      state.open_search_modal ? state.open_search_modal = false : state.open_search_modal = true;
     },
+    open_list_modal(state) {
+      state.open_list_modal = true
+    },
+    close_list_modal(state) {
+      state.open_list_modal = false
+    }
   },
   actions: {},
   getters: {},
