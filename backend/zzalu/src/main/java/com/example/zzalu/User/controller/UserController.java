6453a0d3eb7779017b57c3d9cc76d52afe9094b3
강혜성 <fortuna3co@gmail.com>
2023-02-02 13:58:59 +0900

    Deleted backend/.idea/modules/com.example.zzalu.main.iml, backend/.idea/modules/zzalu.zzalu.main.iml, backend/.idea/.gitignore, backend/.idea/compiler.xml, backend/.idea/gradle.xml, backend/.idea/jarRepositories.xml, backend/.idea/misc.xml, backend/.idea/modules.xml, backend/.idea/uiDesigner.xml, backend/.idea/vcs.xml, backend/.idea/zzalu.iml, backend/zzalu/gradle/wrapper/gradle-wrapper.jar, backend/zzalu/gradle/wrapper/gradle-wrapper.properties, backend/zzalu/src/main/java/com/example/zzalu/Redis/config/RedisConfig.java, backend/zzalu/src/main/java/com/example/zzalu/TitleHakwon/controller/CommentController.java, backend/zzalu/src/main/java/com/example/zzalu/TitleHakwon/controller/TitleHakwonController.java, backend/zzalu/src/main/java/com/example/zzalu/TitleHakwon/dto/CommentRequest.java, backend/zzalu/src/main/java/com/example/zzalu/TitleHakwon/dto/CommentResponse.java, backend/zzalu/src/main/java/com/example/zzalu/TitleHakwon/dto/ReplyCommentRequest.java, backend/zzalu/src/main/java/com/example/zzalu/TitleHakwon/dto/ReplyCommentResponse.java, backend/zzalu/src/main/java/com/example/zzalu/TitleHakwon/entity/Comment.java, backend/zzalu/src/main/java/com/example/zzalu/TitleHakwon/entity/CommentLike.java, backend/zzalu/src/main/java/com/example/zzalu/TitleHakwon/entity/DeleteCommentStatus.java, backend/zzalu/src/main/java/com/example/zzalu/TitleHakwon/entity/ReplyComment.java, backend/zzalu/src/main/java/com/example/zzalu/TitleHakwon/entity/TitleHakwon.java, backend/zzalu/src/main/java/com/example/zzalu/TitleHakwon/repository/CommentLikeRepository.java, backend/zzalu/src/main/java/com/example/zzalu/TitleHakwon/repository/CommentRepository.java, backend/zzalu/src/main/java/com/example/zzalu/TitleHakwon/repository/ReplyCommentRepository.java, backend/zzalu/src/main/java/com/example/zzalu/TitleHakwon/repository/TitleHackwonRepository.java, backend/zzalu/src/main/java/com/example/zzalu/TitleHakwon/service/CommentService.java, backend/zzalu/src/main/java/com/example/zzalu/TitleHakwon/service/TitleHakwonService.java, backend/zzalu/src/main/java/com/example/zzalu/User/controller/UserController.java, backend/zzalu/src/main/java/com/example/zzalu/User/model/Member.java, backend/zzalu/src/main/java/com/example/zzalu/User/repository/MemberRepository.java, backend/zzalu/src/main/java/com/example/zzalu/amazonS3/config/AmazonS3Config.java, backend/zzalu/src/main/java/com/example/zzalu/amazonS3/upLoader/S3Uploader.java, backend/zzalu/src/main/java/com/example/zzalu/RootContext.java, backend/zzalu/src/main/java/com/example/zzalu/ZzaluApplication.java, backend/zzalu/src/main/resources/static/index.html, backend/zzalu/src/main/resources/templates/index.html, backend/zzalu/src/test/java/com/example/zzalu/ZzaluApplicationTests.java, backend/zzalu/.gitignore, backend/zzalu/build.gradle, backend/zzalu/gradlew, backend/zzalu/gradlew.bat, backend/zzalu/settings.gradle, backend/README.md

--- backend/zzalu/src/main/java/com/example/zzalu/User/controller/UserController.java
+++
@@ -1,4 +0,0 @@
-package com.example.zzalu.User.controller;
-
-public class UserController {
-}
--- backend/zzalu/src/main/java/com/example/zzalu/User/repository/MemberRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.zzalu.User.repository;
-
-import com.example.zzalu.User.model.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<Member, String> {
-
-    Member findMemberByMemberId (String memberId);
-}
