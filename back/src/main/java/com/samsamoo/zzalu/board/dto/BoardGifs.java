6d57eb6f918304474f640e6236bb4ae9eb86d977
nana-moon <yeokyung502@gmail.com>
2023-02-04 16:43:46 +0900

    Feature: 특정 짤에 대한 보드 정보 불러오기

---
+++ back/src/main/java/com/samsamoo/zzalu/board/dto/BoardGifs.java
@@ -0,0 +1,13 @@
+package com.samsamoo.zzalu.board.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+public class BoardGifs {
+    private List<BoardInfo> boards;
+}
---
+++ back/src/main/java/com/samsamoo/zzalu/board/dto/GifInfo.java
@@ -0,0 +1,11 @@
+package com.samsamoo.zzalu.board.dto;
+
+import lombok.Getter;
+
+@Getter
+public class GifInfo {
+    private Long id;
+
+    // 짤 제목 추가?
+    private String gifPath;
+}
