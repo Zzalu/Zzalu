09ed675a806b70724e7ea0ea0d877b90c8afd7e0
giokim12 <109334986+giokim12@users.noreply.github.com>
2023-02-06 17:50:33 +0900

    Feat: 회원가입 이메일 받기까지 완료
    이메일 입력 후 회원가입 성공해야됨

--- front/src/store/modules/userStore.js
+++ front/src/store/modules/userStore.js
@@ -1,7 +1,4 @@
-// import { postSignup } from "@/api/userSignup";4
-import axios from 'axios'
-
-const API_URL = 'http://127.0.0.1:3000'
+import { checkUsername, checkNickname, checkEmail } from "@/api/userAccount";
 
 const userStore = {
   namespaced: true,
@@ -9,50 +6,54 @@ const userStore = {
   //   isLogin: false,
   // }),
   state: {
-    // 사용자 정보
-    username: null,
-    nickname: null,
-    email: null,
-    profile_image: '',
-    introduction: null,
-    following: [],
-    followers: [],
-
-    // 보드
-
-    // 수상기록
-
-
+    temp: {}
   },
   mutations: {
-    SIGN_UP(state, new_user) {
-      state.new_user = new_user
+    SAVE_USER_TEMP(state, credentialsData) {
+      state.temp = credentialsData
+    },
+    SAVE_EMAIL_TEMP(state, credentialsEmailCode) {
+      state.temp.email = credentialsEmailCode.email
+      state.temp.code = credentialsEmailCode.code
     }
   },
   getters: {},
   actions: {
-    signUp(context, credentials) {
-      const username = credentials.username
-      const nickname = credentials.nickname
-      const password = credentials.password
-      const passwordCheck = credentials.passwordCheck
-
-      axios({
-        method: 'post',
-        url: `${API_URL}/members/signup`,
-        data: {
-          username, 
-          nickname, 
-          password, 
-          passwordCheck
-        }
-      })
-      .then(res => {
-        context.commit('SIGN_UP', res.data)
-      })
-      .catch(err => console.log(err))
+    // 아이디 중복확인
+    uniqueUsernameAction: async (commit, username) => {
+        // console.log(username);
+        const response = await checkUsername({"username": username});
+        // console.log(response)
+        return response
+    },
+    // 닉네임 중복확인
+    uniqueNicknameAction: async (commit, nickname) => {
+        // console.log(nickname);
+        const response = await checkNickname({"nickname": nickname});
+        // console.log(response)
+        return response
+    },
+    // 이메일 중복확인
+    sendEmailAction: async (commit, email) => {
+      console.log(email);
+      const data = JSON.stringify({"userEmail": email})
+      const response = await checkEmail(data);
+      console.log("거의끝났어",response)
+      return response
+    },
+    // 회원정보 잠시 세이브
+    signupFirstAction(context, credentialsData) {
+      context.commit('SAVE_USER_TEMP', credentialsData)
+    },
+    // 회원정보 세이브 2
+    signupSecondAction(context, credentialsEmailCode) {
+      console.log("두번째 요청까지는 잘 들어옴")
+      context.commit('SAVE_EMAIL_TEMP', credentialsEmailCode)
     }
-  }
+
+
+    
+  },
 };
 
 export default userStore;
