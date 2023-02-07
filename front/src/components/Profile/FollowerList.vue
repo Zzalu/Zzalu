3d5b13fcb7b55d8cfe3eefe1e0631e631376b7b5
nana-moon <yeokyung502@gmail.com>
2023-02-08 03:38:31 +0900

    Feature: 팔로우, 팔로잉 리스트, 팔로, 언팔 구현

--- front/src/components/Profile/FollowerList.vue
+++ front/src/components/Profile/FollowerList.vue
@@ -1,17 +1,37 @@
 <template>
   <h1>팔로워 리스트</h1>
-  <follower-list-item/>
+  <div class="items">
+    <div v-for="(Followers, i) in FollowerListItemData" :key="i">
+    <follower-list-item
+    :Followers="Followers"
+    />
+  </div>
+
+  </div>
+  
 </template>
 
 <script>
 import FollowerListItem from "@/components/Profile/Item/FollowerListItem"
+import { useStore } from "vuex";
+import { computed } from "@vue/runtime-core";
 
 export default {
-  name: "FollowingList",
+  name: "FollowerList",
+  setup() {
+    const store = useStore();
+    const FollowerListItemData = computed(
+      () => store.state.followStore.follower_list
+    );
+    return {
+      FollowerListItemData
+    }
+
+  },
   components: {
     FollowerListItem
   }
-}
+};
 </script>
 
 <style>
--- front/src/components/Profile/Item/FollowerListItem.vue
+++ front/src/components/Profile/Item/FollowerListItem.vue
@@ -1,10 +1,53 @@
 <template>
   <h1>er item</h1>
+  <div>{{ this.Followers }}</div>
+  <div>{{ this.id }}</div>
+  <div>{{ this.username }}</div>
+  <button @click="follow_request">{{ this.followState }}</button>
 </template>
 
 <script>
-export default {
+import { useStore } from 'vuex';
+import { toRefs } from 'vue';
 
+export default {
+  name : "FollowerListItem",
+  props : {
+    Followers : Object,
+  },
+  setup(props) {
+    const { id } = toRefs(props.Followers)
+    const { username } = toRefs(props.Followers)
+    const { nickname } = toRefs(props.Followers)
+    const { profilePath } = toRefs(props.Followers)
+    const { followState } = toRefs(props.Followers)
+    const store = useStore();
+
+    const follow_request = () => {
+      if (followState.value == false) {
+        store.dispatch("followStore/requsetFollow", id.value)
+        .then((res) => {
+          console.log(res)
+          followState.value = true;
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+      } else {
+        store.dispatch("followStore/requestUnfollow", id.value)
+        .then((res) => {
+          console.log(res)
+          followState.value = false;
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+      }
+    };
+    return {
+      id, username, nickname, profilePath, followState, follow_request
+    }
+  },
 }
 </script>
 
