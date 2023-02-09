ce5334116a1c863dbbbf66fba91fb48d90695c80
chaeeun <codms1104@naver.com>
2023-02-10 03:04:24 +0900

    design: 제목학원, 수상내역 다크모드

--- front/src/components/TitleCompetition/AwardRecordList.vue
+++ front/src/components/TitleCompetition/AwardRecordList.vue
@@ -3,22 +3,22 @@
     <div class="medal-board">
       <font-awesome-icon icon="fa-solid fa-medal" class="text-zz-gold h-8" />
       <div class="medal-title">금상</div>
-      <div>0회</div>
+      <div>23.01.17</div>
     </div>
     <div class="medal-board">
       <font-awesome-icon icon="fa-solid fa-medal" class="text-zz-silver h-8" />
       <div class="medal-title">은상</div>
-      <div>0회</div>
+      <div>23.01.16</div>
     </div>
     <div class="medal-board">
       <font-awesome-icon icon="fa-solid fa-medal" class="text-zz-bronze h-8" />
       <div class="medal-title">동상</div>
-      <div>0회</div>
+      <div>23.01.14</div>
     </div>
     <div class="medal-board">
       <font-awesome-icon icon="fa-solid fa-medal" class="text-zz-bronze h-8" />
       <div class="medal-title">동상</div>
-      <div>0회</div>
+      <div>23.01.11</div>
     </div>
   </div>
 </template>
