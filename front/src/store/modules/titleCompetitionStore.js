391349c99df9d24311edfecd4b5317d5d15dff2b
chaeeun <codms1104@naver.com>
2023-02-12 18:13:36 +0900

    feature: socket 새로운 댓글 store에 저장하기

--- front/src/store/modules/titleCompetitionStore.js
+++ front/src/store/modules/titleCompetitionStore.js
@@ -23,6 +23,10 @@ const titleCompetitionStore = {
       comment_id: '',
       nickname: '',
     },
+
+    // 소켓 관련
+    socket_comment_cnt: 0,
+    socket_comments: [],
   }),
   getters: {
     getDate: (state) => state.open_date,
@@ -92,6 +96,16 @@ const titleCompetitionStore = {
     },
     DELETE_COMMENT(state, comment_index) {
       state.comments.splice(comment_index, 1);
+    },
+    ADD_SOCKET_COMMENT_CNT(state) {
+      state.socket_comment_cnt += 1;
+    },
+    ADD_SOCKET_COMMENT(state, comment) {
+      state.socket_comments.push(comment);
+    },
+    DELETE_SOCKET_DATA(state) {
+      state.socket_comment_cnt = 0;
+      state.socket_comments = [];
     },
   },
   actions: {
@@ -227,6 +241,19 @@ const titleCompetitionStore = {
     // 삭제 관련
     deleteComment({ commit }, comment_index) {
       commit('DELETE_COMMENT', comment_index);
+    },
+
+    // 소켓 통신 관련
+
+    // 새로운 댓글 추가
+    addSocketCommentCnt({ commit }) {
+      commit('ADD_SOCKET_COMMENT_CNT');
+    },
+    addSocketComment({ commit }, comment) {
+      commit('ADD_SOCKET_COMMENT', comment);
+    },
+    deleteSocketData({ commit }) {
+      commit('DELETE_SOCKET_DATA');
     },
   },
 };
