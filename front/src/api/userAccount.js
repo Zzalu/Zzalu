212dd85f3a3bc05aa612365324b10b79ef6fa213
강혜성 <fortuna3co@gmail.com>
2023-02-16 11:47:32 +0900

    Update userAccount.js

--- front/src/api/userAccount.js
+++ front/src/api/userAccount.js
@@ -5,19 +5,19 @@ const form_api = formApiInstance();
 
 // 회원가입 (1)
 const requestRegister = (payload,res,err) => {
-    return api.post(`signup/members/signup`, payload)
+    return api.post(`/members/signup`, payload)
     .then(res).catch(err)
 }
 
 // 로그인 (2)
 const requestLogin = (payload, res, err) => {
-    return api.post(`members/login`, payload)
+    return api.post(`/members/login`, payload)
     .then(res).catch(err)
 }
 
 // 비번변경 (3)
 const requestChangePassword  = (payload, res, err) => {
-    return api.patch(`members/pass`, payload)
+    return api.patch(`/members/pass`, payload)
     .then(res).catch(err)
 }
 
@@ -28,12 +28,12 @@ const getProfileUser = (username, success, fail) => {
 
 // 아이디 중복체크 (5)
 const checkUsername = (payload) => {
-    return api.get(`members/exists`, {params: payload})
+    return api.get(`/members/exists`, {params: payload})
 }
 
 // 닉네임 중복체크 (6)
 const checkNickname = (payload) => {
-    return api.get(`members/exists`, {params: payload})
+    return api.get(`/members/exists`, {params: payload})
 }
 
 // 회원정보변경 (7)
@@ -59,25 +59,25 @@ const requestDelete = (pwd, res, err) => {
 
 // 매니저권한 요청 (12)
 const requestManager = (res, err) => {
-    auth_api.post(`members/manager`).then(res).catch(err)
+    auth_api.post(`/members/manager`).then(res).catch(err)
 }
 
 // -----------------------------------------------------------
 // 회원가입시 이메일인증 (2-1)
 const checkEmail = (data,res,err) => {
-    return api.post(`mail/signup`, data )
+    return api.post(`/mail/signup`, data )
     .then(res).catch(err)
 }
 
 // 비번변경시 이메일인증 (2-2)
 const changePasswordEmail  = (payload, res, err) => {
-    return api.post(`members/pass`, payload)
+    return api.post(`/members/pass`, payload)
     .then(res).catch(err)
 }
 
 // 아이디찾기시 이메일 인증 (2-3)
 const requestUsername = (data,res,err) => {
-    return api.post(`mail/username`, data )
+    return api.post(`/mail/username`, data )
     .then(res).catch(err)
 }
 
