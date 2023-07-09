be63296202418b7c8930b69d404e2295879eb1c4
giokim12 <109334986+giokim12@users.noreply.github.com>
2023-07-09 23:45:38 +0900

    Feat: 회원 관련 api 수정

--- front/src/views/Accounts/AccountDeleteView.vue
+++ front/src/views/Accounts/AccountDeleteView.vue
@@ -1,27 +1,25 @@
 <template>
   <only-go-back-top-nav-bar></only-go-back-top-nav-bar>
-  <!-- <ask-again></ask-again> -->
-  <delete-complete></delete-complete>
+  <ask-again></ask-again>
+  <!-- <delete-complete></delete-complete> -->
   <main-bottom-nav></main-bottom-nav>
 </template>
 
 <script>
-import OnlyGoBackTopNavBar from '../../components/Common/NavBar/OnlyGoBackTopNav.vue'
-import MainBottomNav from '../../components/Common/NavBar/MainBottomNav.vue'
-// import AskAgain from "../../components/Accounts/Delete/AskAgain.vue"
-import DeleteComplete from "../../components/Accounts/Delete/DeleteComplete.vue"
+import OnlyGoBackTopNavBar from "../../components/Common/NavBar/OnlyGoBackTopNav.vue";
+import MainBottomNav from "../../components/Common/NavBar/MainBottomNav.vue";
+import AskAgain from "../../components/Accounts/Delete/AskAgain.vue";
+// import DeleteComplete from "../../components/Accounts/Delete/DeleteComplete.vue"
 
 export default {
   name: "AccountDeleteView",
   components: {
     OnlyGoBackTopNavBar,
-    // AskAgain,
-    DeleteComplete,
-    MainBottomNav
-  }
-}
+    AskAgain,
+    // DeleteComplete,
+    MainBottomNav,
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
